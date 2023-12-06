c60ed0561dff110472ba8cf1da8bdcf30aa3d2ee
hyojelee <hyojelee1004ss@gmail.com>
2023-12-05 21:09:28 -0800

    added admin

--- myEmployeeList/models.py
+++ myEmployeeList/models.py
@@ -9,11 +9,11 @@ class TeamMember(models.Model):
   email = models.EmailField(blank=False, null= False)
   phone_number = models.IntegerField(blank=False, null= False)
 
-  CHOICES = [('option1','label 1'), ('option2','label 2')] 
+  # CHOICES = [('option1','label 1'), ('option2','label 2')] 
 
-  some_field = forms.CharField(
-    required = True,label = "where is it???",
-    widget=forms.RadioSelect(choices=CHOICES))
+  # some_field = forms.CharField(
+  #   required = True,label = "where is it???",
+  #   widget=forms.RadioSelect(choices=CHOICES))
 
   def __str__(self):
     return self.first_name + self.last_name + self.email + self.phone_number
