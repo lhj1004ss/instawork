fe9bf11aacaeef5235fb73e610132d2cdd179bde
hyojelee <hyojelee1004ss@gmail.com>
2023-12-06 17:59:47 -0800

    fixed db and post request

--- myEmployeeList/models.py
+++ myEmployeeList/models.py
@@ -7,7 +7,7 @@ class TeamMember(models.Model):
   first_name = models.CharField(max_length=25, blank=False, null= False)
   last_name = models.CharField(max_length=25, blank=False, null= False)
   email = models.EmailField(blank=False, null= False)
-  phone_number = models.IntegerField(blank=False, null= False)
+  phone_number = models.CharField(max_length=25,blank=False, null= False)
 
   def __str__(self):
-    return self.first_name + self.last_name + self.email + self.phone_number
+    return self.first_name + " " + self.last_name
--- myEmployeeList/views.py
+++ myEmployeeList/views.py
@@ -4,12 +4,7 @@ from myEmployeeList.form import TeamMemberForm
 from .models import TeamMember
 
 # Create your views here.
-# teamMembers = [
-#   {'id': 1, 'firstName': 'Adrien', 'lastName': 'Olczck', 'email': 'adrien@instawork.com', 'phone':'415-310-1619', 'admin': True },
-#   {'id': 2, 'firstName': 'Charlene', 'lastName': 'Pham', 'email': 'charlene@instawork.com', 'phone':'415-310-1619', 'admin': False },
-#   {'id': 3, 'firstName': 'Benson', 'lastName': 'Mach', 'email': 'benson@instawork.com', 'phone':'415-310-1619', 'admin': False },
-#   {'id': 4, 'firstName': 'Den', 'lastName': 'Petrie', 'email': 'den@instawork.com', 'phone':'415-310-1619', 'admin': False },
-#   ]
+
 
 
 def index(request):
@@ -20,17 +15,10 @@ def add(request):
   context = {'form': form}
 
   if request.method == "POST":
-    # print('request', request)
-    # first_name = request.POST.get('first_name')
-    # last_name = request.POST.get('last_name')
-    # email = request.POST.get('email')
-    # phone_number = request.POST.get('phone_number')
-    data = TeamMember(request.POST)
-    # data = TeamMemberForm(first_name = first_name, last_name = last_name, email=email, phone_number = phone_number)
-    # print(first_name,last_name,email,phone_number)
-    # data.save()
-    data.save()
-    return redirect('/')
+    form = TeamMemberForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return redirect('/')
 
   return render(request, "add.html", context)
 
